6ebefa50aea0c78646569621f8d2a89e700b2eec
Rebeca Garcia <rebe.ga87@gmail.com>
2024-09-23 14:38:51 +0200

    add small button component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import SmallButton from "../../components/buttons/smallButton/SmallButton";
 
 const Home = () => {
     return (
-        <div>
+        <div style={{display:"flex", flexDirection:"column",gap:'15px', paddingTop:'20px'}}>
+            <SmallButton text="Default" />
+            <SmallButton type="variant2" text="variant2"/>
+            <SmallButton type="variant3" text="variant3"/>
+            <SmallButton type="disable" text="disable"/>
         </div>
     );
 };
