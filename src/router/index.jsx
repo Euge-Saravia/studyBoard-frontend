c683839275a59b4abd5fff2803b308d1d8a63d5b
Laura Gil <laura.gilcara@gmail.com>
2024-09-23 11:37:57 +0200

    import new pages

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,10 +1,11 @@
-import React from "react";
 import { createBrowserRouter } from "react-router-dom";
+import Home from "../pages/home/Home";
+import Layout from "../layout/Layout";
 
 export const router = createBrowserRouter([
     {
         path: "/",
-        element: <Layout />,
+        element: <Layout/>,
         children: [
             {
                 path: "/",
