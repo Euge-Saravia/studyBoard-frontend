11e7b1867db0ab97414010f5640df67b5ae86700
Eugenia Saravia <63812926+Euge-Saravia@users.noreply.github.com>
2024-09-26 15:35:11 +0200

    Chanching parent component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,18 +1,8 @@
 
-import { useState } from "react";
-import ProfileImg from "../../components/profileImg/ProfileImg";
-import Sidebar from "../../components/sidebar/Sidebar";
-
 const Home = () => {
-    const [profileImage, setProfileImage] = useState(""); 
 
-  const handleImageUpload = (imageUrl) => {
-    setProfileImage(imageUrl); 
-  };
     return (
         <div>
-            <Sidebar />
-            <ProfileImg profileImage={profileImage} onImageUpload={handleImageUpload}/>
         </div>
     );
 };
