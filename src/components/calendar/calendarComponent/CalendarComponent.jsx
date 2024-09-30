ad46383a1c223680563b619730d0c54bcfcaab53
Betty Lopez <elisabetlopezpons@gmail.com>
2024-09-30 11:48:41 +0200

    Finish calendar component, calendar and day logic applied, add style and structure

--- src/components/calendar/calendarComponent/CalendarComponent.jsx
+++ src/components/calendar/calendarComponent/CalendarComponent.jsx
@@ -3,10 +3,51 @@ import WeekCalendar from "../weekCalendar/WeekCalendar.jsx"
 import ViewSelector from "../../buttons/viewSelector/ViewSelector.jsx"
 import GroupImage from "../../sidebar/boardImage/GroupImage.jsx";
 import TodayCard from "../todayCard/TodayCard.jsx";
+import PostIt from "../../board/postIt/PostIt.jsx";
+import { isSameDay, format } from "date-fns";
+import { es } from "date-fns/locale";
 
 const CalendarComponent = () => {
+    const postits = [
+        {
+            type: "perano",
+            title: "JWT",
+            text: "Lorem ipsum"
+        },
+        {
+            type: "wheat",
+            title: "Spring",
+            text: "Lorem ipsum ipsum lorem"
+        },
+        {
+            type: "rose",
+            title: "Security",
+            text: "Lorem ipsum"
+        },
+        {
+            type: "green-brut",
+            title: "Arrays",
+            text: "Lorem ipsum ipsum lorem "
+        },
+    ]
+
+    const today = new Date();
+
+    const todayCalendar = () => {
+        const isToday = isSameDay(today, new Date());
+        return {
+            dayNumber: format(today, 'd', { locale: es }),
+            dayWeek: format(today, 'EEEE', { locale: es }),
+            dayYear: format(today, 'yyy', { locale: es}),
+            dayMonth: format(today, 'MMM', { locale: es}),
+            isToday
+        };
+    };
+
+    const { dayNumber, dayWeek, dayYear, dayMonth, isToday } = todayCalendar(); 
+
   return (
-    <div>
+    <div className="calendar-comp">
         <section className="calendar-section">
             <div className="top-navigation">
                 <GroupImage onClick="" profileImage="" position="position"/>
@@ -17,9 +58,19 @@ const CalendarComponent = () => {
                 <WeekCalendar />
             </div>
             <div className="today-cal">
-                <TodayCard month="sep" year="2024" date="27" dayWeek="viernes"/>
+                <TodayCard month={dayMonth} year={dayYear} date={dayNumber} dayWeek={dayWeek} isToday={isToday}/>
             </div>
             <div className="linebt"></div>
+            <section className="cal-post-it">
+                {postits.map((postit, index) => (
+                    <PostIt 
+                        key={index}
+                        type={postit.type} 
+                        title={postit.title}
+                        text={postit.text}/>
+                ))
+                }
+            </section>
         </section>
     </div>
   )
