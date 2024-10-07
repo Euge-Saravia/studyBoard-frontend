968c0d9e881321b1fb8e485118d83f9b675f72bd
Isa Afonso <126961475+IsaLagu@users.noreply.github.com>
2024-10-07 22:52:55 +0200

    add login test

--- src/pages/login/LoginPage.test.jsx
+++ src/pages/login/LoginPage.test.jsx
@@ -1,6 +1,6 @@
 import { render, screen, fireEvent, waitFor, act } from "@testing-library/react";
 import { describe, it, expect, vi } from "vitest";
-import SignUpPage from "./SignUpPage";
+import LoginPage from "./LoginPage";
 import { BrowserRouter } from "react-router-dom";
 import { useAuth } from "../../hooks/useAuth";
 import usePost from "../../hooks/usePost";
@@ -8,7 +8,7 @@ import usePost from "../../hooks/usePost";
 vi.mock("../../hooks/usePost");
 vi.mock("../../hooks/useAuth");
 
-describe("SignUpPage Integration Test", () => {
+describe("LoginPage Integration Test", () => {
   it("should display validation errors if form fields are invalid", async () => {
     const mockExecutePost = vi.fn();
 
@@ -27,25 +27,25 @@ describe("SignUpPage Integration Test", () => {
 
     render(
       <BrowserRouter>
-        <SignUpPage />
+        <LoginPage />
       </BrowserRouter>
     );
 
     // Simulate form submission without entering any data
-    const submitButton = screen.getByText("Registrarse");
+    const submitButton = screen.getByText("Iniciar sesión");
     fireEvent.click(submitButton);
 
     // Check if validation errors appear
-    expect(await screen.findAllByText("Este campo es obligatorio")).toHaveLength(3);
+    expect(await screen.findAllByText("Este campo es obligatorio")).toHaveLength(2);
   });
 
-  it("should display error message if the API call fails", async () => {
+  it("should display an error message if the API call fails", async () => {
     const mockExecutePost = vi.fn();
 
     usePost.mockReturnValue({
       data: null,
       loading: false,
-      error: "Registro fallido",
+      error: "Inicio de sesión fallido",
       executePost: mockExecutePost,
     });
 
@@ -57,30 +57,24 @@ describe("SignUpPage Integration Test", () => {
 
     render(
       <BrowserRouter>
-        <SignUpPage />
+        <LoginPage />
       </BrowserRouter>
     );
 
     // Simulate user input
-    fireEvent.input(screen.getByPlaceholderText("Nombre"), {
-      target: { value: "John Doe" },
-    });
     fireEvent.input(screen.getByPlaceholderText("Correo electrónico"), {
       target: { value: "john.doe@example.com" },
     });
     fireEvent.input(screen.getByPlaceholderText("Contraseña"), {
       target: { value: "password123" },
     });
-    fireEvent.input(screen.getByPlaceholderText("Repite la contraseña"), {
-      target: { value: "password123" },
-    });
 
     // Simulate form submission
-    const submitButton = screen.getByText("Registrarse");
+    const submitButton = screen.getByText("Iniciar sesión");
     fireEvent.click(submitButton);
 
-    // Ensure the post function was called with the correct data
-    expect(await screen.findByText("Error: Registro fallido")).toBeInTheDocument();
+    // Check if error message is displayed
+    expect(await screen.findByText("Error: Inicio de sesión fallido")).toBeInTheDocument();
   });
 
   it("should submit the form and handle the API response", async () => {
@@ -100,41 +94,26 @@ describe("SignUpPage Integration Test", () => {
 
     render(
       <BrowserRouter>
-        <SignUpPage />
+        <LoginPage />
       </BrowserRouter>
     );
 
     // Simulate user input
-    fireEvent.input(screen.getByPlaceholderText("Nombre"), {
-      target: { value: "John Doe" },
-    });
     fireEvent.input(screen.getByPlaceholderText("Correo electrónico"), {
       target: { value: "john.doe@example.com" },
     });
     fireEvent.input(screen.getByPlaceholderText("Contraseña"), {
       target: { value: "password123" },
     });
-    fireEvent.input(screen.getByPlaceholderText("Repite la contraseña"), {
-      target: { value: "password123" },
-    });
 
     // Simulate form submission
-    const submitButton = screen.getByText("Registrarse");
+    const submitButton = screen.getByText("Iniciar sesión");
     fireEvent.click(submitButton);
 
     // Ensure the post function was called with the correct data
     act(async () => {
       await waitFor(() =>
         expect(mockExecutePost).toHaveBeenCalledWith({
-          name: "John Doe",
-          email: "john.doe@example.com",
-          password: "password123",
-        })
-      );
-
-      await waitFor(() =>
-        expect(mockExecutePost).toHaveBeenCalledWith({
-          name: "John Doe",
           email: "john.doe@example.com",
           password: "password123",
         })
