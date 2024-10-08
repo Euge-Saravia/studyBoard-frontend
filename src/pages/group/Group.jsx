dc700ba6cca48e5b591679f53cb25e32b29b61b7
BettyLopo <elisabetlopezpons@gmail.com>
2024-10-08 12:57:05 +0200

    Mediaqueries added to group, adjustements on styling, spacing group navigation, adjustments and mediaqueries to calendar container and about page

--- src/pages/group/Group.jsx
+++ src/pages/group/Group.jsx
@@ -1,19 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import CalendarComponent from "../../components/calendar/calendarComponent/CalendarComponent";
 import GroupNav from "../../components/group/groupNav/GroupNav";
 import BoardTagsContainer from "../../components/board/boardTagsContainer/BoardTagsContainer";
 import "./group.scss";
+import Board from "../../components/board/Board";
 
 const Group = ({ name }) => {
     //comprobar si user actual es creador
     const isCreator = true;
     const [activeView, setActiveView] = useState("Boards");
+    const [isDesktop, setIsDesktop] = useState(window.innerWidth > 1024);
 
     const handleViewChange = (view) => {
         if (view !== activeView) {
             setActiveView(view);
         }
     };
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsDesktop(window.innerWidth > 1024);
+        };
+
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, [isDesktop]);
+
+
 
     const components = {
         'Boards' : <BoardTagsContainer key="boards" isCreator={isCreator} name={name} />,
@@ -22,10 +35,21 @@ const Group = ({ name }) => {
 
     return (
         <>
-        <GroupNav onViewChange={handleViewChange}/>
-        <section className={activeView}>
-            {components[activeView]} 
-        </section>
+        <div>
+        {!isDesktop && <>
+            <GroupNav onViewChange={handleViewChange}/>
+            <section className={activeView}>
+                {components[activeView]} 
+            </section>
+            </>
+        }
+        </div>
+        {isDesktop &&
+            <div className="group-deskt">
+                <BoardTagsContainer key="boards" isCreator={isCreator} name={name} />
+                <CalendarComponent key="calendar"/>
+            </div>
+        }
         </>
     );
 };
