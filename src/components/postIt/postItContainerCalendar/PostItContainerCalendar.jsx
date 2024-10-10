0534684a697a4105cbb27112ed0c1b76fc953454
Laura Gil <laura.gilcara@gmail.com>
2024-10-10 11:24:43 +0200

    add fetch postits by date, and on date change

--- src/components/postIt/postItContainerCalendar/PostItContainerCalendar.jsx
+++ src/components/postIt/postItContainerCalendar/PostItContainerCalendar.jsx
@@ -11,80 +11,79 @@ const PostItContainerCalendar = ({ date, groupId }) => {
     const [postits, setPostits] = useState([]);
     const [loader, setLoader] = useState(false);
     const [selectId, setSelectId] = useState(null);
-    const [endpoint, setEndpoint] = useState(`${GET_POST_IT_BY_DATE.replace("${groupId}", groupId)}${date}`);
+    const endpoint = `${GET_POST_IT_BY_DATE.replace(
+        "${groupId}",
+        groupId
+    )}${date}`;
     const { authToken } = useAuth();
-    const { data, error, fetch: fetchData } = useFetch(
+    const {
+        data,
+        error,
+        fetch: fetchData,
+    } = useFetch(
         endpoint,
         {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${authToken}`,
-          },
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${authToken}`,
+            },
         },
         false
-      );
+    );
 
     useEffect(() => {
-        setEndpoint(
-            `${GET_POST_IT_BY_DATE.replace("${groupId}", groupId)}${date}`
-        );
-    }, [date]);
+        if (groupId && date) {
+            setLoader(true);
+            fetchData().then(() => {
+                setTimeout(() => {
+                    setLoader(false);
+                }, 500);
+            });
+        }
+    }, [date, groupId]);
 
     useEffect(() => {
-        setLoader(true);
-        fetchData(
-            endpoint,
-            {
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: `Bearer ${authToken}`,
-                },
-            },
-            true
-        );
-        setTimeout(() => {
-            if (data) {
-                setPostits(data || []);
-                setLoader(false);
-            }
-          }, "500");
-    }, [endpoint]);
+        if (data) {
+            setPostits(data);
+        }
+    }, [data]);
 
     const selectPostIt = postits.find((postit) => postit.id === selectId);
     return (
         <div className="postit-section">
-            { loader && <p className="messages">Buscando Post-its...</p> }
-            { !loader && error && <p className="messages">{error}</p> }
+            {loader && <p className="messages">Buscando Post-its...</p>}
+            {!loader && error && <p className="messages">{error}</p>}
             <div className="expanded-post-it">
-                { !loader && !error &&  
-                <AnimatePresence>
-                    {selectId && selectPostIt && (
-                        <PostItExpand
-                            layoutId={`calendar-${selectId}`}
-                            type={selectPostIt.color}
-                            title={selectPostIt.title}
-                            text={selectPostIt.textContent}
-                            onClick={() => setSelectId(null)}
-                        />
-                    )}
-                </AnimatePresence>
-                }
+                {!loader && !error && (
+                    <AnimatePresence>
+                        {selectId && selectPostIt && (
+                            <PostItExpand
+                                layoutId={`calendar-${selectId}`}
+                                type={selectPostIt.color}
+                                title={selectPostIt.title}
+                                text={selectPostIt.textContent}
+                                onClick={() => setSelectId(null)}
+                            />
+                        )}
+                    </AnimatePresence>
+                )}
             </div>
             <div className="postit-container">
-            { !loader && !error && <>
-                {postits.map((postit, index) => (
-                    <PostIt
-                        layoutId={`calendar-${postit.id}`}
-                        key={index}
-                        type={postit.color}
-                        title={postit.title}
-                        text={postit.textContent}
-                        onClick={() => setSelectId(postit.id)}
-                    />
-                ))}
-            </>}
+                {!loader && !error && (
+                    <>
+                        {postits.map((postit, index) => (
+                            <PostIt
+                                layoutId={`calendar-${postit.id}`}
+                                key={index}
+                                type={postit.color}
+                                title={postit.title}
+                                text={postit.textContent}
+                                onClick={() => setSelectId(postit.id)}
+                            />
+                        ))}
+                    </>
+                )}
             </div>
         </div>
     );
