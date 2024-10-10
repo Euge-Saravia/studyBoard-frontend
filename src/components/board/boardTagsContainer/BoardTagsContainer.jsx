aff15b257c370a23bb2a7a274ff7d006f365cf23
Rebeca Garcia <rebe.ga87@gmail.com>
2024-10-10 12:37:17 +0200

    try render new board

--- src/components/board/boardTagsContainer/BoardTagsContainer.jsx
+++ src/components/board/boardTagsContainer/BoardTagsContainer.jsx
@@ -9,92 +9,76 @@ import { READ_BY_ID } from "../../../config";
 import useFetch from "../../../hooks/useFetch";
 
 const BoardTagsContainer = ({ id }) => {
-  const { authToken } = useAuth();
-  const endpoint = READ_BY_ID.replace("${id}", id);
-  const [openCardIndex, setOpenCardIndex] = useState(null);
-  const [isCreator, setIsCreator] = useState(false);
-  const [boards, setBoards] = useState([]);
+    const { authToken } = useAuth();
+    const endpoint = READ_BY_ID.replace("${id}", id);
+    const [openCardIndex, setOpenCardIndex] = useState(null);
+    const [isCreator, setIsCreator] = useState(false);
+    const [boards, setBoards] = useState([]);
 
-  const fetchOptions = {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      ...(authToken ? { Authorization: `Bearer ${authToken}` } : {}),
-    },
-  };
+    const fetchOptions = {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            ...(authToken ? { Authorization: `Bearer ${authToken}` } : {}),
+        },
+    };
 
-  const { data, loading, error } = useFetch(endpoint, fetchOptions, !!authToken);
+    const { data, loading, error, fetch: fetchData } = useFetch(endpoint, fetchOptions, !!authToken);
 
-  useEffect(() => {
-    if (data) {
-      setIsCreator(data.isCreator);
-      setBoards(data.boards || []);
-    }
-  }, [data]);
+    useEffect(() => {
+        if (data) {
+            setIsCreator(data.isCreator);
+            setBoards(data.boards || []);
+        }
+    }, [data]);
 
-  if (loading)
+    if (loading) return <LoadingModal />;
+    if (error) return <AlertModal title="Error:" errorText={error.message} />;
+    if (!data) return <AlertModal errorText="No hay datos disponibles" />;
+
+
+    const toggleBoard = (index) => {
+        setOpenCardIndex(openCardIndex === index ? null : index);
+    };
+
+    const handleBoardCreated = () => {
+        setOpenCardIndex(null)
+        fetchData()
+        .then(()=> setBoards(data.boards || []))
+    };
+
     return (
-      <div>
-        {" "}
-        <LoadingModal />{" "}
-      </div>
+        <section className="boards">
+            <h1>{data.groupName}</h1>
+            <section className="tags">
+                {isCreator && (
+                    <CreateBoard
+                        key={0}
+                        isOpen={openCardIndex === 0}
+                        toggleBoard={() => toggleBoard(0)}
+                        onBoardCreated={handleBoardCreated}
+                        id={id}
+                    />
+                )}
+                {boards.length > 0 ? (
+                    data.boards.map((board, index) => {
+                        return (
+                            <Board
+                                key={index + 1}
+                                color={board.color}
+                                name={board.title}
+                                isOpen={openCardIndex === index + 1}
+                                toggleBoard={() => toggleBoard(index + 1)}
+                                id={board.id}
+                            />
+                        );
+                    })
+                ) : (
+                    <p>No hay tableros disponibles</p>
+                )}
+            </section>
+        </section>
     );
-  if (error)
-    return (
-      <div>
-        {" "}
-        <AlertModal title="Error:" errorText={error.message} />{" "}
-      </div>
-    );
-  if (!data) {
-    return (
-      <div>
-        {" "}
-        <AlertModal errorText="No hay datos disponibles" />{" "}
-      </div>
-    );
-  }
-
-  const toggleBoard = (index) => {
-    setOpenCardIndex(openCardIndex === index ? null : index);
-  };
-
-  const handleBoardCreated = (newBoard) => {
-    setBoards([...boards, newBoard]);
-  };
-
-  return (
-    <section className="boards">
-      <h1>{data.groupName}</h1>
-      <section className="tags">
-        {isCreator && (
-          <CreateBoard
-            key={0}
-            isOpen={openCardIndex === 0}
-            toggleBoard={() => toggleBoard(0)}
-            onBoardCreated={handleBoardCreated}
-            id={id}
-          />
-        )}
-        {data.boards && data.boards.length > 0 ? (
-          data.boards.map((board, index) => {
-            return (
-              <Board
-                key={index + 1}
-                color={board.color}
-                name={board.title}
-                isOpen={openCardIndex === index + 1}
-                toggleBoard={() => toggleBoard(index + 1)}
-                id={board.id}
-              />
-            );
-          })
-        ) : (
-          <p>No hay tableros disponibles</p>
-        )}
-      </section>
-    </section>
-  );
 };
 
 export default BoardTagsContainer;
