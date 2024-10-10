9979492677db188db7fe0acd13ea1f43415972b3
Eugenia Saravia <63812926+Euge-Saravia@users.noreply.github.com>
2024-10-10 15:42:15 +0200

    Implementig join a group

--- src/components/group/groupCard/GroupCard.jsx
+++ src/components/group/groupCard/GroupCard.jsx
@@ -1,24 +1,57 @@
 import { useNavigate } from "react-router-dom";
-import JoinButton from "../../buttons/joinButton/JoinButton"
-import GroupLabel from "../../labels/groupLabel/GroupLabel"
-import "./groupCard.scss"
+import JoinButton from "../../buttons/joinButton/JoinButton";
+import GroupLabel from "../../labels/groupLabel/GroupLabel";
+import "./groupCard.scss";
+import { useAuth } from "../../../hooks/useAuth";
+import useFetch from "../../../hooks/useFetch";
 
-const GroupCard = ({ groupId, title, categories}) => {
-    const navigate = useNavigate();
-	
-	const handleRedirect = () => {
-		navigate(`/group/${title}`, { state: { data: groupId} })
+const GroupCard = ({ groupId, title, categories, isMember }) => {
+  const navigate = useNavigate();
+  const { authToken } = useAuth();
 
-	}
+  // Hook para hacer la petición de unirse al grupo
+  const {
+    fetch: joinGroup,
+    loading,
+    error,
+  } = useFetch(
+    `/group/join/${groupId}`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${authToken}`,
+      },
+    },
+    false
+  );
+
+  const handleRedirect = () => {
+    navigate(`/group/${title}`, { state: { data: groupId } });
+  };
+
+  const handleJoin = async () => {
+    try {
+      await joinGroup(); // Realiza la petición al backend
+      console.log("Te has unido al grupo con éxito");
+    } catch (err) {
+      console.error(err);
+      console.log("Hubo un error al unirse al grupo");
+    }
+  };
+
+  console.log("Is member: " + isMember);
   return (
     <div className="groupcard-container">
-        <div className="title-icons">
-            <h5 className="grouptitle" onClick={handleRedirect}>{title}</h5>
-            <JoinButton />
-        </div>
-        <GroupLabel categories={categories}/>
+      <div className="title-icons">
+        <h5 className="grouptitle" onClick={handleRedirect}>
+          {title}
+        </h5>
+        <JoinButton onClick={handleJoin} disabled={loading} initialJoined={isMember}/>
+      </div>
+      <GroupLabel categories={categories} />
     </div>
-  )
-}
+  );
+};
 
-export default GroupCard
+export default GroupCard;
