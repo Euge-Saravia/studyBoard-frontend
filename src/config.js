ee97909d384712d79a34ba6f261e320b4e242075
Laura Gil <laura.gilcara@gmail.com>
2024-10-12 22:12:43 +0200

    github login moved to its own hook

--- src/config.js
+++ src/config.js
@@ -4,6 +4,8 @@ export const USER_LOGIN = "/api/users/login";
 
 export const USER_REGISTER = "/api/users/register";
 
+export const GITHUB_AUTH = "/auth/github/callback?code=${code}"
+
 export const GROUP_BASE= "/group"
 
 export const CREATE_GROUP = GROUP_BASE + "/add"
@@ -37,4 +39,12 @@ export const USER_PROFILE = "/api/users/me";
 export const UPDATE_PROFILE_AVATAR = USER_PROFILE + "/avatar"
 
 export const UPDATE_PROFILE_NAME = USER_PROFILE + "/name"
+
+export const LOGIN_PAGE = "http://localhost:5173/login"
+
+export const SIGNUP_PAGE = "http://localhost:5173/signup"
+
+export const GITHUB_CLIENT_ID = "Ov23liP3FvUqB3NF84xO"
+
+export const GITHUB_REDIRECT = "https://github.com/login/oauth/authorize?client_id=${clientID}&redirect_uri=${redirectURI}"
 
---
+++ src/hooks/useGithubAuth.jsx
@@ -0,0 +1,63 @@
+import { useEffect, useState } from 'react';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import useFetch from './useFetch';
+import { GITHUB_AUTH, SIGNUP_PAGE, GITHUB_CLIENT_ID, GITHUB_REDIRECT } from '../config';
+
+const useGithubAuth = (login, page) => {
+    const [githubLoading, setGithubLoading] = useState(false);
+    const [url, setGithubUrl] = useState("");
+    const [searchParams] = useSearchParams();
+    const navigate = useNavigate();
+    const {
+        data: githubData,
+        error: githubError,
+        fetch: executeGithubFetch,
+    } = useFetch(url, { method: "POST" }, false);
+
+    const handleGithubLogin = () => {
+        window.location.href = GITHUB_REDIRECT
+            .replace("${clientID}", GITHUB_CLIENT_ID)
+            .replace("${redirectURI}", page);
+    };
+
+    useEffect(() => {
+        const code = searchParams.get('code');
+        if (code) {
+            setGithubLoading(true);
+            setGithubUrl(GITHUB_AUTH.replace('${code}', code)); 
+        }
+    }, [searchParams]);
+
+    useEffect(() => {
+        const fetchGitHubData = async () => {
+            const code = searchParams.get('code');
+            if (code) {
+                setGithubLoading(true);
+                setGithubUrl(GITHUB_AUTH.replace('${code}', code)); 
+                try {
+                    await executeGithubFetch({
+                        headers: { 'Content-Type': 'application/json' },
+                    });
+                } catch (error) {
+                    console.error('Error fetching GitHub data:', error);
+                } finally {
+                    setGithubLoading(false);
+                }
+            }
+        };
+        fetchGitHubData();
+    }, [url, executeGithubFetch]);
+
+    useEffect(() => {
+        if (githubData && githubData.token) {
+            login(githubData.token);
+        } else if (githubError) {
+            console.error('Error en la autenticación con GitHub:', githubError);
+        }
+        setGithubLoading(false);
+    }, [githubData, githubError, login, navigate]);
+
+    return { handleGithubLogin, githubLoading };
+}
+
+export default useGithubAuth
