149c08d6272681b9f5caabbc754a9dba04bd29dd
Laura Gil <laura.gilcara@gmail.com>
2024-10-12 12:19:41 +0200

    fix sidebar position when changing pages after scrolling

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -7,51 +7,60 @@ import "./layout.scss";
 import { AuthProvider } from "../hooks/useAuth";
 
 const Layout = () => {
-  const location = useLocation();
-  const noNavRoutes = ["/", "/login", "/signup"];
-  const [sidebarIsOpen, setSidebarIsOpen] = useState(false);
-  const [isDesktop, setIsDesktop] = useState(window.innerWidth > 1024);
+    const location = useLocation();
+    const noNavRoutes = ["/", "/login", "/signup"];
+    const [sidebarIsOpen, setSidebarIsOpen] = useState(false);
+    const [isDesktop, setIsDesktop] = useState(window.innerWidth > 1024);
 
-  const showNavSidebar = !noNavRoutes.some((route) => {
-    const regex = new RegExp(`^${route}(\\/|\\?|$)`);
-    return regex.test(location.pathname);
-  });
+    const showNavSidebar = !noNavRoutes.some((route) => {
+        const regex = new RegExp(`^${route}(\\/|\\?|$)`);
+        return regex.test(location.pathname);
+    });
 
-  useEffect(() => {
-    if (isDesktop) {
-      setSidebarIsOpen(isDesktop);
-    }
+    useEffect(() => {
+        if (isDesktop) {
+            setSidebarIsOpen(isDesktop);
+        }
 
-    const handleResize = () => {
-      setIsDesktop(window.innerWidth > 1024);
+        const handleResize = () => {
+            setIsDesktop(window.innerWidth > 1024);
+        };
+
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, [isDesktop]);
+
+    const toggleSidebar = () => {
+        setSidebarIsOpen(!sidebarIsOpen);
     };
 
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, [isDesktop]);
+    const backgroundClass = noNavRoutes.includes(location.pathname)
+        ? "blue-background"
+        : "white-background";
 
-  const toggleSidebar = () => {
-    setSidebarIsOpen(!sidebarIsOpen);
-  };
-
-  const backgroundClass = noNavRoutes.includes(location.pathname) ? "blue-background" : "white-background";
-
-  return (
-    <AuthProvider>
-      {showNavSidebar && <NavBar />}
-      {showNavSidebar && <Sidebar isOpen={sidebarIsOpen} toggleSidebar={toggleSidebar} />}
-      <main className={backgroundClass}>
-        <Outlet />
-      </main>
-      {showNavSidebar && (
-        <motion.div
-          className={`overlay ${sidebarIsOpen ? "open" : "closed"}`}
-          animate={sidebarIsOpen ? { opacity: 0.7 } : { opacity: 0 }}
-          transition={{ duration: 0.5 }}
-        />
-      )}
-    </AuthProvider>
-  );
+    return (
+        <AuthProvider>
+            {showNavSidebar && <NavBar />}
+            <aside className="sidebar-container">
+                {showNavSidebar && (
+                    <Sidebar
+                        isOpen={sidebarIsOpen}
+                        toggleSidebar={toggleSidebar}
+                    />
+                )}
+            </aside>
+            <main className={backgroundClass}>
+                <Outlet />
+            </main>
+            {showNavSidebar && (
+                <motion.div
+                    className={`overlay ${sidebarIsOpen ? "open" : "closed"}`}
+                    animate={sidebarIsOpen ? { opacity: 0.7 } : { opacity: 0 }}
+                    transition={{ duration: 0.5 }}
+                />
+            )}
+        </AuthProvider>
+    );
 };
 
 export default Layout;
